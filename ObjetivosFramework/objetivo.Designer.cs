4c6cbaffca9b415e2fc7fc6ad58c4149dcf5f6b4
AnalystRoyal <analystroyalis@gmail.com>
2018-11-01 18:56:01 -0600

    Version 1.0.0

---
+++ ObjetivosFramework/objetivo.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace ObjetivosFramework
+{
+    partial class objetivo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(objetivo));
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(85, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(398, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Objetivos Framework";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-4, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(586, 62);
+            this.panel1.TabIndex = 42;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 24);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "Framework:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(14, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 24);
+            this.label3.TabIndex = 45;
+            this.label3.Text = "Objetivo:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 141);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(525, 308);
+            this.groupBox1.TabIndex = 47;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingreso de datos";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 117);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(494, 180);
+            this.dataGridView1.TabIndex = 57;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(149, 60);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(209, 32);
+            this.comboBox2.TabIndex = 56;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(149, 16);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(209, 32);
+            this.comboBox1.TabIndex = 55;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(151, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 58);
+            this.button1.TabIndex = 48;
+            this.button1.Text = "Guardar";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.Location = new System.Drawing.Point(237, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 58);
+            this.button2.TabIndex = 49;
+            this.button2.Text = "Eliminar";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button3.Location = new System.Drawing.Point(323, 69);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 58);
+            this.button3.TabIndex = 50;
+            this.button3.Text = "Actualizar";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // objetivo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 461);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "objetivo";
+            this.Text = "ObjetivosFramework";
+            this.Load += new System.EventHandler(this.ObjetivosFramework_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ ObjetivosFramework/objetivo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.Odbc;
+using Connection;
+
+namespace ObjetivosFramework
+{
+    public partial class objetivo : Form
+    {
+        public objetivo()
+        {
+            InitializeComponent();
+        }
+
+        private void ObjetivosFramework_Load(object sender, EventArgs e)
+        {
+            //String stringpad = "server=localhost;user id=root;database=auditoria";
+            String stringpad = "dsn=auditoria;UID=root;PWD=;";
+            Connection.cone.dllconnectionDatabase connection = new Connection.cone.dllconnectionDatabase(stringpad);
+
+            if (connection.OpenConnection())
+            {
+                /*We open the connection*/
+                MessageBox.Show("We are connected");
+
+                try
+                {
+                    
+                    OdbcConnection cone = new OdbcConnection(stringpad);
+                    string selectQuery = "select * from metodologias;";
+                    string selectQuery1 = "select * from objetivo;";
+                    
+                    OdbcCommand command = new OdbcCommand(selectQuery,cone);
+                    OdbcCommand command1 = new OdbcCommand(selectQuery1, cone);                    
+
+                    OdbcDataAdapter mysqldt = new OdbcDataAdapter(command);
+                    DataTable dt = new DataTable();
+                    mysqldt.Fill(dt);
+
+                    OdbcDataAdapter mysqldt1 = new OdbcDataAdapter(command1);
+                    DataTable dt1 = new DataTable();
+                    mysqldt1.Fill(dt1);
+
+                    comboBox1.ValueMember = "idMetodologias";
+                    comboBox1.DisplayMember = "Nombre";
+                    comboBox1.DataSource = dt;
+
+                    comboBox2.ValueMember = "idObjetivo";
+                    comboBox2.DisplayMember = "Descripcion";
+                    comboBox2.DataSource = dt1;
+                                       
+                    connection.OpenConnection();
+                    dataGridView1.DataSource = connection.llenarDataGridView("select metodologias.Nombre, objetivo.Descripcion from metodologias inner join objetivo inner join metodologias_has_objetivo on metodologias_has_objetivo.metodologias_idMetodologias = metodologias.idMetodologias and metodologias_has_objetivo.objetivo_idObjetivo = objetivo.idObjetivo order by metodologias.Nombre;");
+                    connection.CloseConnection();
+
+                }
+                catch (Exception ex)
+                {
+
+
+                    MessageBox.Show(ex.Message);
+                }
+
+
+            }
+            else
+            {
+                /*We have an error*/
+                MessageBox.Show("Error");
+            }
+            
+        }
+
+
+
+
+        }
+    }
